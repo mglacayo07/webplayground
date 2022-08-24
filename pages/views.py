c94e6ea27f4edcd040c73b1b719cfddae67e6e35
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-24 18:13:06 -0500

    Usando decoradores de identificación

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,22 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
-
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
+
+
+class StaffRequiredMixin(object):
+    """
+    Este mixin requerirá que el usuario sea miembro del staff
+    """
+    @method_decorator(staff_member_required)
+    def dispatch(self,request, *args, **kwargs):
+        return super(StaffRequiredMixin,self).dispatch(request,*args,**kwargs)
+
 
 # Lista
 # todas las Pages
@@ -18,13 +30,14 @@ class PageDetailView(DetailView):
     model = Page
 
 
-# Vista para crear un page
+@method_decorator(staff_member_required, name="dispatch")
 class PageCreate(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
 
+@method_decorator(staff_member_required, name="dispatch")
 class PageUpdate(UpdateView):
     model = Page
     form_class = PageForm
@@ -35,6 +48,7 @@ class PageUpdate(UpdateView):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
 
+@method_decorator(staff_member_required, name="dispatch")
 class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
