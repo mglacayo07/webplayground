716dd68ab663f9f9a4303b8bf88d2cfc23793421
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-24 15:26:18 -0500

    Vistas CRUD con CBV: CreateView

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,24 @@
-from .models import Page
-
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Page
 
 
+# Lista todas las Pages
 class PageListView(ListView):
     model = Page
     paginate_by = 100
 
 
+# Se puede ver el detallado de una page
 class PageDetailView(DetailView):
     model = Page
+
+
+# Vista para crear un page
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title','content','order']  # campos que son editables
+    success_url = reverse_lazy('pages:pages')
