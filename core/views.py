8a7d37b674327fa17d0e78446d49ce8996f9acc8
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-24 10:03:34 -0500

    Revisión App [Core] CBV y TemplateView

--- core/views.py
+++ core/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def home(request):
-    return render(request, "core/home.html")
 
-def sample(request):
-    return render(request, "core/sample.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['title'] = "Mi super Web Playground"
+    #     return context
+
+    # Este metodo sobre escribe la respuesta de si mismta
+    def get(self,request, *args, **kwargs):
+        kw = {
+            'title': "Mi Super Web Playground"
+        }
+        return render(request,self.template_name, kw)
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
