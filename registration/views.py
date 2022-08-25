a7e77fc1eab59c53f1f8d9459f5d0f3f3e25738e
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-24 20:45:33 -0500

    Email obligatorio al registrarse

--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
 
 class SingUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     #success_url = reverse_lazy('login')
     template_name = 'registration/singup.html'
 
@@ -16,9 +17,11 @@ class SingUpView(CreateView):
         # Modificar en tiempo real el formulario
         form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
                                                                 'placeholder': 'Nombre de usuario'})
-        form.fields['password1'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
+        form.fields['email'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
+                                                                'placeholder': 'Email'})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2',
                                                                 'placeholder': 'Contraseña'})
-        form.fields['password2'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control mb-2',
                                                                 'placeholder': 'Confirmación de contraseña'})
 
         return form
