f05a192e0d36ae5f0df17b27db96fe543dc2c64d
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-24 20:03:25 -0500

    Registro con CBV (1)

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
     # Paths de Auth
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
 ]
