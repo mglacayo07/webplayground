717d7f290cbd9c408aed164244635b14a40fbc57
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-08-24 20:34:18 -0500

    Registro con CBV (2)

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-
+from django import forms
 
 class SingUpView(CreateView):
     form_class = UserCreationForm
@@ -10,3 +10,15 @@ class SingUpView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login') + '?register'
+
+    def get_form(self, form_class=None):
+        form = super(SingUpView, self).get_form()
+        # Modificar en tiempo real el formulario
+        form.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
+                                                                'placeholder': 'Nombre de usuario'})
+        form.fields['password1'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
+                                                                'placeholder': 'Contraseña'})
+        form.fields['password2'].widget = forms.TextInput(attrs={'class': 'form-control mb-2',
+                                                                'placeholder': 'Confirmación de contraseña'})
+
+        return form
